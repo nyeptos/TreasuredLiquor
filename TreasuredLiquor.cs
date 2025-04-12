b1f45bcbded45ec9edd442561c9e26d76b1fcaaf
Nipi256 <72137181+Nipi256@users.noreply.github.com>
2025-04-12 18:26:59 +0900

    一部画像に変更。 バージョンアップに伴ってbuild.txt変更

--- TreasuredLiquor.cs
+++ TreasuredLiquor.cs
@@ -1,8 +1,7 @@
 using Terraria.ModLoader;
 
 namespace TreasuredLiquor
-{
-    // Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+{ 
     public class TreasuredLiquor : Mod
     {
 
